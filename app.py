0af4c0a612e74d95294abd32f4911c6704fba4c0
Uord <deivxd@gmail.com>
2019-03-11 18:56:24 +0100

    aa

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import request
+import time
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello():
+    return 'Touched for the very first time.'
+
+
+@app.route('/user-agent')
+def user_info():
+    return f'browser: {request.user_agent.browser} os: {request.user_agent.platform} '
+
+
+@app.route('/request')
+def request_info():
+    return f'request method: {request.method} url: {request.url} headers: {request.headers}'
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
