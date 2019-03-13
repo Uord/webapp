36cc77c851fc577e8fa5fd691bfda03631c75456
Uord <deivxd@gmail.com>
2019-03-13 18:06:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 from flask import Flask
 from flask import request
-
 import json
 
-
-
 app = Flask(__name__)
-
-
 
 @app.route('/')
 def hello():
@@ -21,8 +16,13 @@ def method():
 def show_data():
     if request.headers['Content-Type'] == 'application/json':
         data = request.get_json()
-        
         return json.dumps(data).encode('utf8')
+
+@app.route('/pretty_print_name', methods = ['POST'])
+def print_name():
+        data = request.get_json()
+        data2 = json.dumps(data).encode('utf8')
+        return f'Na imię mu {data2[1]}, a nazwisko jego {data2[2]}'
 
 if __name__ == '__main__':
     app.run(debug=False)
