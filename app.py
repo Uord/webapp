cafd09946709ba0619b283849a48c30faa59c1c3
Uord <deivxd@gmail.com>
2019-03-13 12:58:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
 from flask import Flask
-from flask import request
-import time
+
 app = Flask(__name__)
 
 
 
 @app.route('/')
 def hello():
-    return 'Siemano, Ola!'
-
-
-@app.route('/user-agent')
-def user_info():
-    return f'browser: {request.user_agent.browser} os: {request.user_agent.platform} '
-
-
-@app.route('/request')
-def request_info():
-    return f'request method: {request.method} url: {request.url} headers: {request.headers}'
+    return 'Hello, World!'
 
 
 if __name__ == '__main__':
