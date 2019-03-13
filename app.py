9ee3563af62c9b60c505e364b43fec6e43fc69eb
Uord <deivxd@gmail.com>
2019-03-13 17:36:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,7 @@ def show_data():
     if request.headers['Content-Type'] == 'application/json':
         ##data -json.dumps(settings.CONSTANT_TUPLE, encoding='utf-8', ensure_ascii=False)
         data = request.get_json()
-        data2 = json.dumps(data,encoding='utf-8')
-        return data2
+        return str(data)
 
 if __name__ == '__main__':
     app.run(debug=False)
