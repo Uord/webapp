9c269d671ea0b25980291872a70f73e40741ed41
Uord <deivxd@gmail.com>
2019-04-06 23:07:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -67,19 +67,19 @@ class ValidationForm(Form):
 
 
 def get_tracks():
-    form = ValidationForm(request.args)
+    form1 = ValidationForm(request.args)
 
-    if not form.validate():
-        return jsonify(error=form.errors)
+    if not form1.validate():
+        return jsonify(error=form1.errors)
 
-    per_page = form.data['per_page'] or -1
+    per_page = form1.data['per_page'] or -1
     limit = per_page
 
-    page = form.data['page'] or 0
+    page = form1.data['page'] or 0
     page_index = page - 1
     offset = page_index * per_page
 
-    if form.data['artist']:
+    if form1.data['artist']:
         db = get_db()
         data = db.execute(
         'SELECT tracks.name, artists.name AS artist FROM tracks '
@@ -88,7 +88,7 @@ def get_tracks():
         'WHERE artists.name = ? '
         'ORDER by tracks.name COLLATE NOCASE '
         'LIMIT ? OFFSET ?;',
-        (form.data['artist'], limit, offset)).fetchall()
+        (form1.data['artist'], limit, offset)).fetchall()
         data2 = []
         for x in data:
             #data2[x[0]] = x[1]
@@ -104,17 +104,24 @@ def get_tracks():
 
 def post_track():
     db = get_db()
-    new_track = request.get_json()
-
+    #new_track = request.get_json()
+    album_id = request.form['album_id']
+    media_type_id = request.form['media_type_id']
+    genre_id = request.form['genre_id']
+    name = request.form['name']
+    composer = request.form['composer']
+    milliseconds = request.form['milliseconds']
+    bytess = request.form['bytes']
+    price = request.form['price']
     
-    album_id = new_track.get('album_id')
+    """ album_id = new_track.get('album_id')
     media_type_id = new_track.get('media_type_id')
     genre_id = new_track.get('genre_id')
     name = new_track.get('name')
     composer = new_track.get('composer')
     milliseconds = new_track.get('milliseconds')
     bytess = new_track.get('bytes')
-    price = new_track.get('price')
+    price = new_track.get('price') """
 
     if album_id is None:
         raise InvalidUsage(f'missing "AlbumID" in request data')
@@ -134,9 +141,10 @@ def post_track():
         raise InvalidUsage(f'missing "UnitPrice" in request data')
 
     db.execute('''INSERT INTO tracks (name, albumid, mediatypeid, genreid, composer, milliseconds, bytes, unitprice)
-                    VALUES (?,?,?,?,?,?,?,?)''',(name, album_id, media_type_id, genre_id, composer, milliseconds, bytess, price))
+                VALUES (?,?,?,?,?,?,?,?)''',(name, album_id, media_type_id, genre_id, composer, milliseconds, bytess, price))
+    db.commit()
     data = db.execute('''SELECT * FROM tracks
-                            WHERE trackid = (SELECT MAX(trackid)  FROM tracks)''').fetchone()
+                        WHERE trackid = (SELECT MAX(trackid)  FROM tracks)''').fetchone()
     return jsonify(data), 200
 
 
